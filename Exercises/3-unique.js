a6037326a41b51bc285609785022638186300ea5
m1guelten <footballMiguelTen@gmail.com>
2021-11-20 14:06:34 +0200

    task1-4

--- Exercises/3-unique.js
+++ Exercises/3-unique.js
@@ -1,8 +1,16 @@
 'use strict';
 
-// Create and return a new array without duplicate elements
+// Create and return a new array without duplicate elementsA
 // Don't modify initial array
 
-const unique = (array) => [];
-
+const unique = (array) => {
+    const newArr = [];
+    for (const item of array) {
+      if (!newArr.includes(item)) {
+        newArr.push(item);
+      };
+    };
+    return newArr;
+  };
+  
 module.exports = { unique };
--- Exercises/4-difference.js
+++ Exercises/4-difference.js
@@ -3,6 +3,17 @@
 // Find difference of two arrays
 // elements from array1 that are not includes in array2
 
-const difference = (array1, array2) => [];
+const removeElement = (array, item) => {
+    const index = array.indexOf (item);
+    if (index !== -1) array.splice (index,1);
+  };
+
+const difference = (array1, array2) => {
+    const array0 = array1;
+    for (const item of array2) {
+        removeElement (array0, item);
+    };
+    return array0;
+};
 
 module.exports = { difference };
